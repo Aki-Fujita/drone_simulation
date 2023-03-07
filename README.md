dc6992b6be28c48e948e1f24d9259c30d993a876
Aki-Fujita <f-akihiro@g.ecc.u-tokyo.ac.jp>
2023-03-07 20:14:24 +0900

    ルンゲクッタ手前まで（環境構築含）

---
+++ README.md
@@ -0,0 +1,7 @@
+### 起動するとき
+
+1. トップディレクトリで下記のコマンドを入力  
+   `docker build -t drone_python .`
+2. イメージのビルド（1 のコマンド）が完了したら下記のコマンドでコンテナをスタートする。  
+   `docker-compose up -d`
+3. 続いて vscode の extension である Dev-Container 経由で work ディレクトリを開く
--- work/functions/opt_velocity.py
+++ work/functions/opt_velocity.py
@@ -1,2 +1,22 @@
 # 参考 https://qiita.com/jabberwocky0139/items/e2526fc5ee3b0dbf144b
-def optimal_velocity = 
+import numpy as np
+
+"""
+a: 感応度
+c: 適当な定数（加速しない車間距離がこれで決まる）
+delta_x: 前の車との車間距離
+"""
+
+
+def optimal_velocity(c, a, delta_x):
+
+    # 最適速度関数
+    def V(h):
+        return np.tanh(h - c) + np.tanh(c)
+
+    # 更新式
+    def f(v_n):
+        return a * (V(delta_x) - v_n)
+
+    # ここからルンゲクッタ
+    k1 = f()
