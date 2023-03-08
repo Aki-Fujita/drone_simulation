144307240c288bf524b766f082525d7c6aa4a5a5
Aki-Fujita <f-akihiro@g.ecc.u-tokyo.ac.jp>
2023-03-08 10:25:56 +0900

    最低限のOVを実装

--- work/functions/opt_velocity.py
+++ work/functions/opt_velocity.py
@@ -5,18 +5,25 @@ import numpy as np
 a: 感応度
 c: 適当な定数（加速しない車間距離がこれで決まる）
 delta_x: 前の車との車間距離
+delta_t:時間幅,
+v_n: 現在の速度
 """
 
 
-def optimal_velocity(c, a, delta_x):
-
+def optimal_velocity(c, a, delta_x, delta_t, v_n):
+  
     # 最適速度関数
     def V(h):
         return np.tanh(h - c) + np.tanh(c)
 
     # 更新式
-    def f(v_n):
-        return a * (V(delta_x) - v_n)
+    def f(v):
+        return a * (V(delta_x) - v)
 
     # ここからルンゲクッタ
-    k1 = f()
+    k1 = f(v_n)
+    k2 = f(v_n + k1 * delta_t / 2)
+    k3 = f(v_n + k2 * delta_t / 2)
+    k4 = f(v_n + k3 * delta_t)
+
+    return v_n + delta_t*(k1 + 2*k2 + 2*k3 + k4 ) / 6
--- work/simulation_classes/drones.py
+++ work/simulation_classes/drones.py
@@ -1,10 +1,21 @@
+from functions.opt_velocity import optimal_velocity
+
+
 class Drones:
-    def __init__(self, xcor, ycor) -> None:
+    def __init__(self, xcor, ycor, v_0, a, c) -> None:
         self.xcor = xcor
         self.ycor = ycor
+        self.v_x = v_0
+        self.a = a
+        self.c = c
 
-    def proceed(self):
-        self.xcor += 1
+    def update(self, delta_t, delta_x):
+        print("update")
+        self.v_x = optimal_velocity(self.c, self.a, delta_x, delta_t, self.v_x)
+        self.xcor += self.v_x * delta_t
+        # print("xcor={0}, v={1}".format(self.xcor, self.v_x))
 
-    def print_cors(self):
-        print("xcor={0}, ycor={1}".format(self.xcor, self.ycor))
+    def leader_update(self, delta_t):
+        self.v_x = self.v_x
+        self.xcor += self.v_x * delta_t
+        print(self.xcor)
