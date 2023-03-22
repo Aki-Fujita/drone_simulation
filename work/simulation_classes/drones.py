525e59d6f8810f9452073f4b62334dfc8359c4bf
Aki-Fujita <f-akihiro@g.ecc.u-tokyo.ac.jp>
2023-03-22 11:52:39 +0900

    法定速度を導入してみた（渋滞が消えた）

--- work/simulation_classes/drones.py
+++ work/simulation_classes/drones.py
@@ -1,21 +1,23 @@
 from functions.opt_velocity import optimal_velocity
 
 class Drones:
-    def __init__(self, xcor, ycor, v_0, a, c) -> None:
+    def __init__(self, xcor, ycor, v_0, a, c, legal_speed) -> None:
         self.xcor = xcor
         self.ycor = ycor
         self.v_x = v_0
         self.a = a
         self.c = c
+        self.max_speed = legal_speed
         self.xcorList = [xcor]
         self.v_xList = [v_0]
 
     def update(self, delta_t, delta_x):
+        # self.v_x = min(optimal_velocity(self.c, self.a, delta_x, delta_t, self.v_x), self.max_speed)
         self.v_x = optimal_velocity(self.c, self.a, delta_x, delta_t, self.v_x)
         self.xcor += self.v_x * delta_t
 
     def leader_update(self, delta_t):
-        self.v_x = self.v_x
+        self.v_x += (self.max_speed - self.v_x) * self.a * delta_t
         self.xcor += self.v_x * delta_t
 
     def record(self):
