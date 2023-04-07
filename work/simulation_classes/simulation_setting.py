73387a958682bdaf97672628cda32fbb71133b70
Aki-Fujita <f-akihiro@g.ecc.u-tokyo.ac.jp>
2023-04-07 09:40:28 +0900

    コメント

--- work/simulation_classes/simulation_setting.py
+++ work/simulation_classes/simulation_setting.py
@@ -9,7 +9,7 @@ class SimulationSettings:
         self.simulation_steps = int(TOTAL_TIME / time_step)
         self.scale_factor = scale_factor
         self.is_initialized = False
-        self.result = {}
+        self.drones = []
 
     def initialize(self):
         self.is_initialized = True
@@ -18,12 +18,13 @@ class SimulationSettings:
         if not self.is_initialized:
             raise "initializeメソッドが未実行です"
 
-    def test(self, drone_num):
-        if (drone_num == 1):
+    def test(self):
+        if (self.drone_num == 1):
             leader = Drones(xcor=0, ycor=0, v_0=0, a=1, c=2, legal_speed=2)
             for i in range(self.simulation_steps):
                 leader.leader_update(self.time_step)
                 leader.record()
+            self.drones.append(leader)
 
         else:
             print("testはドローン1台でお願いします。")
